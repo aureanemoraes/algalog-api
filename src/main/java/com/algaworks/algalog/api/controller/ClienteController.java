f3a25d55f0a2e87b2187586458ad9d0cdbfa61b5
Aureane Moraes <aureane.moraes@outlook.com>
2022-09-14 16:35:04 -0300

    implementado flyway, configurado model e criado metodo de lista com o jaakarta persistence

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,19 +14,12 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> lista()
 	{
-		Cliente cliente1 = new Cliente();
-		cliente1.setNome("Cliente Um");
-		cliente1.setEmail("clienteum@email.com");
-		cliente1.setTelefone("96 99999-9999");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setNome("Cliente Dois");
-		cliente2.setEmail("clientedois@email.com");
-		cliente2.setTelefone("96 99999-9999");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
