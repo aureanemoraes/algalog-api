b87f3764179f8ba1458f41005f70e8489ca76763
Aureane Moraes <aureane.moraes@outlook.com>
2022-09-15 12:08:47 -0300

    implementada rota de show estudando 3 formas possiveis de construir o retorno com o ResponseEntity

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,13 +1,11 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +33,24 @@ public class ClienteController {
 	public List<Cliente> lista()
 	{
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<Cliente> show(@PathVariable Long id) {
+		return clienteRepository.findById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
+//		return clienteRepository.findById(id)
+//				.map(cliente -> ResponseEntity.ok(cliente))
+//				.orElse(ResponseEntity.notFound().build());
+		
+//		Optional<Cliente> clienteData = clienteRepository.findById(id);
+//		if (clienteData.isPresent()) {
+//			return ResponseEntity.ok(clienteData.get());
+//		}
+//		
+//		return ResponseEntity.notFound().build();
 	}
 	
 	
