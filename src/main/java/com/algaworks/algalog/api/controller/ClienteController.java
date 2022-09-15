e49af8878b341f77716e8da16fcc81c80aa178ad
Aureane Moraes <aureane.moraes@outlook.com>
2022-09-15 11:54:16 -0300

    utilizando o sprint data jpa para criar respositórios

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -6,20 +6,36 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+//	@Autowired
+//	private ClienteRepository clienteRepository;
+	
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping("/clientes/search")
+	public List<Cliente> search(@RequestParam(name="nome") String pattern) {
+		return clienteRepository.findByNomeContaining(pattern);
+//		return null;
+	}
 	
 	@GetMapping("/clientes")
 	public List<Cliente> lista()
 	{
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
+	
+	
 }
